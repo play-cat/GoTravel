d706dcf3ba5c84c51a2fd3fe83730f3e61db8393
Alex Sivets <localhost83@gmail.com>
2025-03-22 12:29:05 +0200

    v 1.0.0

--- src/js/script.js
+++ src/js/script.js
@@ -1,92 +1,102 @@
-const toggleMenuBtn = document.querySelector('.nav__toggle');
-const menuIcon = document.querySelector('.menu-icon');
-const nav = document.querySelector('#nav');
-const navMobile = document.querySelector('.nav_mobile');
-const scrollTop = document.querySelector('.btn_top');
+window.addEventListener('DOMContentLoaded', () => {
+  const toggleMenuBtn = document.querySelector('.nav__toggle');
+  const menuIcon = document.querySelector('.menu-icon');
+  const nav = document.querySelector('#nav');
+  const navMobile = document.querySelector('.nav_mobile');
+  const scrollTop = document.querySelector('.btn_top');
 
-document.addEventListener('scroll', debounce(handleScroll));
+  document.addEventListener('scroll', debounce(handleScroll));
 
-// burgerMenu
-toggleMenuBtn.addEventListener('click', () => {
-  menuIcon.classList.toggle('menu-icon_active');
-  nav.classList.toggle('nav_mobile');
-  document.body.classList.toggle('no-scroll');
-});
-
-// close mobile menu
-nav.addEventListener('click', e => {
-  if (nav.classList.contains('nav_mobile')) {
-    const link = e.target.closest('a');
-    if (link) {
-      nav.classList.remove('nav_mobile');
-      menuIcon.classList.remove('menu-icon_active');
-      document.body.classList.remove('no-scroll');
-    }
-  }
-});
-
-scrollTop.addEventListener('click', () => {
-  window.scrollTo({
-    top: 0,
-    behavior: 'smooth',
+  // burgerMenu
+  toggleMenuBtn.addEventListener('click', () => {
+    menuIcon.classList.toggle('menu-icon_active');
+    nav.classList.toggle('nav_mobile');
+    document.body.classList.toggle('no-scroll');
   });
-});
 
-// scroll function
-function handleScroll() {
-  toggleMenuBtn.classList.toggle('fixed', window.scrollY < 500);
-  scrollTop.classList.toggle('none', window.scrollY < 800);
-}
+  // close mobile menu
+  nav.addEventListener('click', e => {
+    if (nav.classList.contains('nav_mobile')) {
+      const link = e.target.closest('a');
+      if (link) {
+        nav.classList.remove('nav_mobile');
+        menuIcon.classList.remove('menu-icon_active');
+        document.body.classList.remove('no-scroll');
+      }
+    }
+  });
 
-// debounce helper function
-function debounce(func, timeout = 100) {
-  let timer;
-  return (...args) => {
-    clearTimeout(timer);
-    timer = setTimeout(() => {
-      func.apply(this, args);
-    }, timeout);
-  };
-}
+  scrollTop.addEventListener('click', () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  });
 
-// Slider
-const swiper = new Swiper('.swiper-container', {
-  // Optional parameters
-  loop: true,
-  initialSlide: 3,
-  slidesPerView: 3,
-  centeredSlides: true,
-  spaceBetween: 30,
-  speed: 600,
+  // scroll function
+  function handleScroll() {
+    toggleMenuBtn.classList.toggle('fixed', window.scrollY < 500);
+    scrollTop.classList.toggle('none', window.scrollY < 800);
+  }
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.slider__nav_next',
-    prevEl: '.slider__nav_prev',
-    hideOnClick: true,
-  },
+  // debounce helper function
+  function debounce(func, timeout = 100) {
+    let timer;
+    return (...args) => {
+      clearTimeout(timer);
+      timer = setTimeout(() => {
+        func.apply(this, args);
+      }, timeout);
+    };
+  }
 
-  breakpoints: {
-    // when window width is >= 320px
-    320: {
-      slidesPerView: 1,
-      spaceBetween: 10,
+  // Slider
+  const swiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    loop: true,
+    initialSlide: 3,
+    slidesPerView: 3,
+    centeredSlides: true,
+    spaceBetween: 30,
+    speed: 600,
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.slider__nav_next',
+      prevEl: '.slider__nav_prev',
+      hideOnClick: true,
     },
-    // when window width is >= 480px
-    480: {
-      slidesPerView: 1,
-      spaceBetween: 10,
+
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+      // when window width is >= 640px
+      640: {
+        slidesPerView: 3,
+      },
+      768: {
+        slidesPerView: 3,
+      },
+      1024: {
+        spaceBetween: 20,
+        slidesPerView: 3,
+      },
     },
-    // when window width is >= 640px
-    640: {
-      slidesPerView: 3,
-    },
-    768: {
-      slidesPerView: 3,
-    },
-    1024: {
-      spaceBetween: 20,
-      slidesPerView: 3,
-    },
-  },
+  });
+
+  // Animate On Scroll Library
+  AOS.init({
+    once: true,
+    disable: 'mobile',
+    offset: 120,
+    delay: 50,
+  });
 });
